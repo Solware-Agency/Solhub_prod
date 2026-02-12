3d897fffce16f0662a0c43bf94cde7cd5564dfde
DavidCodec <juegosgeorge0502@gmail.com>
2026-02-12 11:58:44 -0400

    Update useExchangeRate hook to fetch official exchange rate and adjust caching duration: Changed the API endpoint to retrieve the official dollar rate and modified the response handling to return the average rate. Reduced stale time from 1 hour to 30 minutes for improved data freshness.

--- src/shared/hooks/useExchangeRate.ts
+++ src/shared/hooks/useExchangeRate.ts
@@ -1,7 +1,7 @@
 import { useQuery } from '@tanstack/react-query'
 
 const baseUrl = import.meta.env.VITE_API_EXCHANGE_URL
-const fullUrl = `${baseUrl}/USD`
+const fullUrl = `${baseUrl}/v1/dolares/oficial`
 
 export const useExchangeRate = () => {
 	return useQuery({
@@ -10,8 +10,8 @@ export const useExchangeRate = () => {
 			const response = await fetch(fullUrl)
 			if (!response.ok) throw new Error('Network response was not ok')
 			const data = await response.json()
-			return data.rates.VES
+			return data.promedio
 		},
-		staleTime: 1000 * 60 * 60, // 1 hora
+		staleTime: 1000 * 60 * 30, // 30 min
 	})
 }
